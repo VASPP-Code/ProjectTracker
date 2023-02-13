afb70ebb0cd8300e9165ccb01b329b6c6259bd6a
Flat_cat <92849778+Flat-Cat@users.noreply.github.com>
2023-02-13 11:07:04 +0100

    added salt token

--- config/admin.js
+++ config/admin.js
@@ -2,4 +2,7 @@ module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET', '6ee6167e930f2bd75c1e6a5249ec1480'),
   },
+  apiToken: {
+    salt: env('API_TOKEN_SALT', 'd9b0d435829343205432729834502735980e7'),
+  },
 });
