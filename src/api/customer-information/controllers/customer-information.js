79903fcf9a88d7c7aedc41932e9611cc964c6f5b
Flat_cat <92849778+Flat-Cat@users.noreply.github.com>
2023-02-15 13:52:12 +0100

    added functional role

--- src/api/customer-information/controllers/customer-information.js
+++ src/api/customer-information/controllers/customer-information.js
@@ -6,4 +6,18 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::customer-information.customer-information');
+module.exports = createCoreController
+('api::customer-information.customer-information', ({strapi}) => ({
+
+   async exampleAction(ctx) {
+    try {
+      ctx.body = 'ok';
+    } catch (err) {
+      ctx.body = err;
+    }
+  },
+
+
+//    module.exports = createCoreController('api::restaurant.restaurant', ({ strapi }) => ({
+}))
+;
